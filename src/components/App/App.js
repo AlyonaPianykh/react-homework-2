8ac13c9849f4892834091da591a8f4445bd1f0f1
yurashulechka <yurchik652@gmail.com>
2020-03-24 22:47:56 +0200

    Added first route :D

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,6 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-
             {/*<NavLink exact to='/user-page'>*/}
             {/*    <UserPage />*/}
             {/*</NavLink>*/}
