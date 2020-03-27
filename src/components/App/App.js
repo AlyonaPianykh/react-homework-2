086c7d448b733771fe90bea9606b2d4395feeeb5
Marta Serheieva <marta.serheyeva@gmail.com>
2020-03-27 12:47:10 +0200

    Added comments to posts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import { UserPage } from '../../containers/UserPage/UserPage';
 
 import './App.css';
---
+++ src/components/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './LoadingIndicator.scss'
+
+
+const CN = 'loading-center';
+export const LoadingIndicator = () => {
+    return (
+
+            <div className={CN}>
+                <div className={`${CN}__loadingspinner`}>
+                </div>
+            </div>
+
+    )
+};
